8458b95bcd2e4e4c686e37dfc398c4de509ec6f4
James Woolfenden <james.woolfenden@gmail.com>
2019-03-06 00:14:52 +0000

    git config settings

--- devtools/win/git-settings.bat
+++ devtools/win/git-settings.bat
@@ -4,25 +4,25 @@ git config --global merge.tool bc3
 git config --global mergetool.bc3.path "c:/Program Files/Beyond Compare 4/bcomp.exe"
 git config --global user.email "James.Woolfenden@gmail.com"
 git config --global user.name "James Woolfenden"
-core.symlinks=false
-core.autocrlf=true
-core.fscache=true
-color.diff=auto
-color.status=auto
-color.branch=auto
-color.interactive=true
-help.format=html
-rebase.autosquash=true
 
-filter.lfs.required=true
-credential.helper=manager
-filter.lfs.required=true
-
-diff.tool=bc4
-difftool.bc4.cmd="C:/Program Files/Beyond Compare 4/bcomp.exe" "$LOCAL" "$REMOTE"
-merge.tool=bc4
-mergetool.bc4.cmd="C:/Program Files/Beyond Compare 4/bcomp.exe" "$LOCAL" "$REMOTE" "$BASE" "$MERGED"
-mergetool.bc4.trustexitcode=true
-difftool.prompt=false
-alias.unstage=reset HEAD --
-alias.last=log -1 HEAD
+git config --global core.symlinks false
+git config --global core.autocrlf true
+git config --global core.fscache true
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.interactive true
+git config --global help.format html
+git config --global rebase.autosquash true
+git config --global filter.lfs.required true
+git config --global credential.helper manager
+git config --global filter.lfs.required true
+git config --global diff.tool bc4
+git config --global difftool.bc4.cmd "C:/Program Files/Beyond Compare 4/bcomp.exe" "$LOCAL" "$REMOTE"
+git config --global merge.tool bc4
+git config --global mergetool.bc4.cmd "C:/Program Files/Beyond Compare 4/bcomp.exe" "$LOCAL" "$REMOTE" "$BASE" "$MERGED"
+git config --global mergetool.bc4.trustexitcode true
+git config --global difftool.prompt false
+git config --global help.autocorrect 1
+git config --global alias.unstage "reset HEAD --"
+git config --global alias.last "log -1 HEAD"
