091160954228b7ad2a057c763e8e43dfaddacbee
James Woolfenden <James.Woolfenden@gmail.com>
2019-07-11 21:09:46 +0100

    new version

--- devtools/linux/setup-awsvault.sh
+++ devtools/linux/setup-awsvault.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -exo
-VERSION="4.1.0"
+VERSION="4.6.2"
 curl -L https://github.com/99designs/aws-vault/releases/download/v${VERSION}/aws-vault-linux-amd64 > /usr/local/bin/aws-vault
 chmod +x /usr/local/bin/aws-vault
 aws-vault --version
