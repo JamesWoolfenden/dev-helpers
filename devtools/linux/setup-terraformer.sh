97f0d4ff9a3ee712b9b28ed92b528090f29a0178
James Woolfenden <James.Woolfenden@gmail.com>
2019-10-13 11:06:29 +0100

    new tools

---
+++ devtools/linux/setup-terraformer.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+curl -LO https://github.com/GoogleCloudPlatform/terraformer/releases/download/$(curl -s https://api.github.com/repos/GoogleCloudPlatform/terraformer/releases/latest | grep tag_name | cut -d '"' -f 4)/terraformer-linux-amd64
+chmod +x terraformer-linux-amd64
+sudo mv terraformer-linux-amd64 /usr/local/bin/terraformer
+terraformer --version
