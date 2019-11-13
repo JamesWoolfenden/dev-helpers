9d6b3f1193ba908e59258a80b133ea86270261c3
James Woolfenden <James.Woolfenden@slalom.com>
2019-11-13 13:12:12 +0000

    version

--- devtools/linux/setup-terraform.sh
+++ devtools/linux/setup-terraform.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl https://keybase.io/hashicorp/pgp_keys.asc | gpg --import
 apt-get install unzip
-VERSION="0.12.10"
+VERSION="0.12.13"
 TOOL="terraform"
 EDITION="linux_amd64"
 cd /usr/local/bin
