e4224d78a127bfad7016ce351858fa853d9952ac
James Woolfenden <James.Woolfenden@gmail.com>
2020-01-20 09:53:05 +0000

    bump

--- devtools/linux/setup-terraform.sh
+++ devtools/linux/setup-terraform.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl https://keybase.io/hashicorp/pgp_keys.asc | gpg --import
 apt-get install unzip
-VERSION="0.12.17"
+VERSION="0.12.19"
 TOOL="terraform"
 EDITION="linux_amd64"
 cd /usr/local/bin
