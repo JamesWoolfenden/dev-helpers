e5b8ce5e57bd76de18f6066abfb1a4940a457212
James Woolfenden <James.Woolfenden@gmail.com>
2020-01-27 14:34:25 +0000

    cleanup

--- devtools/linux/setup-terraform-docs.sh
+++ devtools/linux/setup-terraform-docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -exo
-VERSION="0.6.0"
+VERSION="0.8.1"
 curl -L "https://github.com/segmentio/terraform-docs/releases/download/v${VERSION}/terraform-docs-v${VERSION}-linux-amd64"  > /usr/local/bin/terraform-docs
 chmod +x /usr/local/bin/terraform-docs
 terraform-docs --version
