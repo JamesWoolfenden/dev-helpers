dccb7f3ca7a0259cc9c89171042b6cc52ce54a2a
James Woolfenden <James.Woolfenden@gmail.com>
2021-05-02 18:00:35 +0100

    refresg

--- devtools/win/setup-terraform-docs.ps1
+++ devtools/win/setup-terraform-docs.ps1
@@ -1,8 +1,9 @@
 $version=lastversion terraform-docs
-$url = "https://github.com/segmentio/terraform-docs/releases/download/v$($version)/terraform-docs-v$($version)-windows-amd64.exe"
+$url = "https://github.com/terraform-docs/terraform-docs/releases/download/v$($version)/terraform-docs-v$($version)-windows-amd64.zip"
+
 Write-Output "$(get-date) - Getting $url"
 
-$installdir ="C:\tools\terraform-docs"
+$installdir ="C:\tools\bin"
 
 if (!(test-path $installdir))
 {
@@ -11,8 +12,10 @@ if (!(test-path $installdir))
 
 try{
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    Invoke-WebRequest -Uri $url -outfile "$installdir\terraform-docs.exe"
-    & "$installdir\terraform-docs.exe" --version
+    Invoke-WebRequest -Uri $url -outfile "$installdir\terraform-docs.zip"
+    cinst unzip
+    unzip -o "$installdir\terraform-docs.zip" -d "$installdir"
+    Remove-Item "$installdir\terraform-docs.zip"
     Write-Output "$(get-date) - Now Update your path"
 }
 catch{
