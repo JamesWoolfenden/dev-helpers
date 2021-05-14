df765d3bb8d2f93f7d1d74909ce3ba3fc2309d2c
James Woolfenden <James.Woolfenden@gmail.com>
2021-05-14 09:51:01 +0100

    use last version and ps params

--- devtools/win/setup-infracost.ps1
+++ devtools/win/setup-infracost.ps1
@@ -1,9 +1,17 @@
-$tool="infracost"
-$version=lastversion $tool
-$zipfile="infracost-windows-amd64.tar.gz"
-$url="https://github.com/infracost/infracost/releases/download/v$version/$zipfile"
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $installdir="C:\tools\bin\"
+)
 
-$installdir ="C:\tools\bin\"
+# requires lastversion and tar to be installed
+
+$tool   = "infracost"
+$version= lastversion $tool
+$zipfile= "infracost-windows-amd64.tar.gz"
+$url    = "https://github.com/infracost/infracost/releases/download/v$version/$zipfile"
+
 remove-item "$installdir\$tool*"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
