50817971286a09aef30fa696d46a218aea7daab9
James Woolfenden <james.woolfenden@gmail.com>
2021-05-27 17:44:43 +0100

    not required

--- devtools/linux/setup-terraform.sh
+++ devtools/linux/setup-terraform.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -exo
-curl https://keybase.io/hashicorp/pgp_keys.asc | gpg --import
 apt-get install unzip
 VERSION=$(lastversion terraform)
 TOOL="terraform"
@@ -8,9 +7,6 @@ EDITION="linux_amd64"
 cd /usr/local/bin
 # Download the binary and signature files.
 wget "https://releases.hashicorp.com/$TOOL/$VERSION/${TOOL}_${VERSION}_${EDITION}.zip"
-
-
-
 
 unzip -o "${TOOL}_${VERSION}_linux_amd64.zip"
 rm "${TOOL}_${VERSION}_linux_amd64.zip"
