5b1ffdfc3785dc3df92f27c48153be106ec3db4c
James Woolfenden <james.woolfenden@gmail.com>
2021-07-08 10:33:27 +0100

    refresh

--- devtools/linux/setup-terraformer.sh
+++ devtools/linux/setup-terraformer.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-curl -LO https://github.com/GoogleCloudPlatform/terraformer/releases/download/$(curl -s https://api.github.com/repos/GoogleCloudPlatform/terraformer/releases/latest | grep tag_name | cut -d '"' -f 4)/terraformer-all-linux-amd64
-chmod +x terraformer-all-linux-amd64
-sudo mv terraformer-all-linux-amd64 /usr/local/bin/terraformer
-terraformer --version
+set -exo
+export PROVIDER={all,google,aws,kubernetes}
+curl -LO https://github.com/GoogleCloudPlatform/terraformer/releases/download/$(curl -s https://api.github.com/repos/GoogleCloudPlatform/terraformer/releases/latest | grep tag_name | cut -d '"' -f 4)/terraformer-${PROVIDER}-linux-amd64
+chmod +x terraformer-${PROVIDER}-linux-amd64
+sudo mv terraformer-${PROVIDER}-linux-amd64 /usr/local/bin/terraformer
