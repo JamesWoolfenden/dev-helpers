665203447f9caf1b23095ad4661cb1da4d517682
James Woolfenden <james.woolfenden@gmail.com>
2021-08-18 15:20:37 +0100

    upgrade

--- devtools/linux/setup-terraform-docs.sh
+++ devtools/linux/setup-terraform-docs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -exo
-VERSION=$(lastversion terraform-docs)
-wget "https://github.com/terraform-docs/terraform-docs/releases/download/v${VERSION}/terraform-docs-v${VERSION}-linux-amd64.tar.gz" --no-check-certificate -O terraform-docs.tar.gz
-tar -xzf terraform-docs.tar.gz terraform-docs
-rm terraform-docs.tar.gz
+lastversion terraform-docs -d --assets
+
+tar -xzf $(find . -name *.tar.gz)
+rm $(find . -name *.tar.gz)
 mv terraform-docs /usr/local/bin/
 chmod +x /usr/local/bin/terraform-docs
 terraform-docs --version
