c5915ed59aadbc09d0cae206a05f50c61cad689d
James Woolfenden <James.Woolfenden@gmail.com>
2021-09-25 11:58:37 +0100

    fix

--- devtools/win/packages/setup-kics.ps1
+++ devtools/win/packages/setup-kics.ps1
@@ -1,18 +1,15 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $installdir="C:\tools\bin\"
+)
+
+# requires lastversion and tar to be installed
 $tool   = "kics"
 $version= lastversion $tool
 $zipfile= "$($tool)_$($version)_windows_x64.zip"
 $url    = "https://github.com/Checkmarx/$tool/releases/download/v$($version)/$zipfile"
-
-Write-Output "$(get-date) - Getting $url"
-
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
-
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
-
 
 $installdir ="C:\tools\bin"
 if (!(test-path $installdir))
@@ -28,10 +25,10 @@ else{
 }
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-write-host "$(get-date) - Getting $version"
 Invoke-WebRequest -Uri $url -outfile $zipfile
-Unzip "$PSScriptRoot\$zipfile" $installdir
-Remove-Item "$PSScriptRoot\$zipfile"
+tar -xvf $zipfile
+Remove-Item $zipfile
+write-output ""$installdir\$tool.exe""
+Move-Item "$tool*" "$installdir\$tool.exe"
 
-& "$installdir\$tool.exe" version
-Write-Output "$(get-date) - Update your path"
+& "$tool.exe" --version
