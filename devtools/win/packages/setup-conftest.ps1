40a9ab332bf42cfc40f691bc5e37ab5524db7bd6
James Woolfenden <James.Woolfenden@gmail.com>
2021-09-25 11:47:33 +0100

    fix

--- devtools/win/packages/setup-conftest.ps1
+++ devtools/win/packages/setup-conftest.ps1
@@ -1,34 +1,24 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $installdir="C:\tools\bin\"
+)
+
+# requires lastversion and tar to be installed
+
 $version= lastversion conftest
 $tool   = "conftest"
 $zipfile= "$($tool)_$($version)_Windows_x86_64.zip"
 $url    = "https://github.com/open-policy-agent/$tool/releases/download/v$($version)/$zipfile"
 
-
-Write-Output "$(get-date) - Getting $url"
-
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
-
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
-
-
-$installdir ="C:\tools\$tool"
-if (!(test-path $installdir))
-{
-    mkdir $installdir
-}
-else{
-    Remove-Item "$installdir\*" -ErrorAction SilentlyContinue
-}
+remove-item "$installdir\$tool*"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-write-host "$(get-date) - getting $version"
 Invoke-WebRequest -Uri $url -outfile $zipfile
-Unzip "$PSScriptRoot\$zipfile" $installdir
-Remove-Item "$PSScriptRoot\$zipfile"
+tar -xvf $zipfile
+Remove-Item $zipfile
+write-output ""$installdir\$tool.exe""
+Move-Item "$tool*" "$installdir\$tool.exe"
 
-& "$installdir\$tool.exe" --version
-Write-Output "$(get-date) - Update your path"
+& "$tool.exe" --version
